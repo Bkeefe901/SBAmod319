0aa3d215df44eff454a70f9218a37eff0b26ec2f
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-17 12:27:19 -0400

    Initialize Schemas

---
+++ models/saleSchema.mjs
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+
+const saleSchema = new mongoose.Schema({});
+
+
+
+export default mongoose.model('Sale', saleSchema);
---
+++ models/shirtSchema.mjs
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+
+const shirtSchema = new mongoose.Schema({});
+
+
+
+export default mongoose.model('Shirt', shirtSchema);
