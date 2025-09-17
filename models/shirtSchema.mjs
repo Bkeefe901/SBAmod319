f8f72dc31e97145c309644401c6cf8f75f1e68ae
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-17 14:00:48 -0400

    Added shirtSchema requirements

--- models/shirtSchema.mjs
+++ models/shirtSchema.mjs
@@ -1,7 +1,40 @@
 import mongoose from "mongoose";
 
 
-const shirtSchema = new mongoose.Schema({});
+const shirtSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    sizes: {
+        type: [String],
+        enum: {values: [
+            'xxl',
+            'xl', 
+            'l', 
+            'm', 
+            's', 
+            'xs'
+        ], 
+        message: 'Size not found' },
+        required: true
+    },
+    colors: {
+        type: [String],
+        enum: { values: [
+            'black',
+            'white',
+            'green',
+            'red',
+            'blue',
+            'grey',
+            'purple',
+            'yellow'
+        ],
+        message: 'Color not available',
+        required: true
+        }
+    },
+    material: String,
+    price: {type: Number, required: true}
+});
 
 
 
