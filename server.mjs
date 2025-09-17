995dbb786fe9ade9ff5584f73f7726096765d3e6
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-17 11:18:34 -0400

    First Commit, Basic Sever Setup

---
+++ server.mjs
@@ -0,0 +1,28 @@
+// Imports
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+
+// Setup
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Middleware
+app.use(express.json());
+
+// Routes
+
+// Global Error-Handling Middleware
+app.use((err, req, res, next)=>{
+    res.status(500).json({msg: `❌ Error - ${err.message}`});
+})
+
+// Listener
+app.listen(PORT, ()=>{
+    console.log(`Server running on Port: ${PORT}`);
+})
+
