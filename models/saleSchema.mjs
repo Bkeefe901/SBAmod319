3df70b84ec45a57a30179ae270e3ac8a5848eb09
Bkeefe901 <bradyjokeefe@gmail.com>
2025-09-18 20:55:06 -0400

    Created validation for saleSchema to make sure items array contained real ids from the shirt collection

--- models/saleSchema.mjs
+++ models/saleSchema.mjs
@@ -1,25 +1,45 @@
 import mongoose from "mongoose";
-import { shirts, users } from '../data/data.mjs';
-import User from './userSchema.mjs';
+
 
 const Schema = mongoose.Schema;
 
 const saleSchema = new Schema({
-    date: {type: Date},
-    items: [{ type: String }], // [{type: String, required: true}],
-    total: {type: Number},
+    date: { type: Date, default: Date.now },
+    items: {
+        type: [mongoose.Schema.Types.ObjectId], // type is strings that must match the _id's from the shirts data in the db
+        required: true,
+        validate: [
+            {
+                validator: function (arr) { // Checks to make sure there is atleast one item in the items array.
+                    return arr.length > 0;
+                },
+                message: "At least one item required"
+            },
+            {
+                validator: async function (arr) { // Checks that the number of items in the array that match the _id's from the shirts collection is equal to the size of the array
+                    const isShirts = await mongoose.model('Shirt').countDocuments({ _id: { $in: arr } });
+
+                    return isShirts == arr.length;
+                },
+                message: "One or more shirt id's are invalid"
+            }
+        ]
+
+
+    }, // [{type: String, required: true}],
+    total: { type: Number }, // I would like to validate this to be equal to the sum of items in the items array *****************************************
     userID: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true,
         validate: {
-            validator: async function(v){
-                const user = await mongoose.model('User').findById(v);
-                return user;
+            validator: async function (v) {
+                const isUser = await mongoose.model('User').findById(v);
+                return isUser;
             },
             message: 'User does not exist.'
         }
-    
+
     }
 });
 
